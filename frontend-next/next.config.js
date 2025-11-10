0a95f42017727c629f870623a5e0164db0ee9b53
anhtranguyen-github <tranguyen.contact@gmail.com>
2025-11-10 22:34:35 +0000

    add config for local run

--- frontend-next/next.config.js
+++ frontend-next/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 //const nextConfig = {basePath: '/grammarpoint',}
-const nextConfig = {}
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/e-api/:path*', destination: 'http://localhost:8000/e-api/:path*' },
+      { source: '/f-api/:path*', destination: 'http://localhost:5100/f-api/:path*' },
+      { source: '/d-api/:path*', destination: 'http://localhost:5200/d-api/:path*' },
+    ]
+  },
+}
 module.exports = nextConfig
-
-
